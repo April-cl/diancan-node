d095375bab6df12d49b2d744ef8510dca584c1be
April-cl <xcl8202@gmail.com>
2021-10-11 00:43:02 +0800

    统一返回给前端JSON格式

--- router/login/login.js
+++ router/login/login.js
@@ -1,7 +1,9 @@
+const result = require('../../config/result.js')
 const router = require('koa-router')()
 
 router.get('/register', async ctx => {
   console.log('login.js')
+  new result(ctx, '请求成功').answer()
 })
 
 module.exports = router.routes()
