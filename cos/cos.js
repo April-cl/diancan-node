73b9726ab7668eded8b02505e2ca56553f97a41b
April-cl <xcl8202@gmail.com>
2021-10-26 01:11:03 +0800

    更改上传文件名

--- cos/cos.js
+++ cos/cos.js
@@ -3,6 +3,13 @@ const multer = require('@koa/multer')
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'upload/image')
+  },
+  filename: (req, file, cb) => {
+    console.log(file)
+    let fileFormat = (file.originalname).split('.')
+    console.log(fileFormat)
+    let num = `${Date.now()}-${Math.floor(Math.random()*10000000)}${'.'}${fileFormat[fileFormat.length-1]}`
+    console.log(num)
   }
 })
 
--- router/merchant-infor/infor.js
+++ router/merchant-infor/infor.js
@@ -2,7 +2,7 @@ const router = require('koa-router')()
 const {upload} = require('../../cos/cos.js')
 
 router.post('/uploadres', upload.single('file'), async ctx => {
-  console.log(ctx)
+  console.log('infor.js')
 })
 
 module.exports = router.routes()
