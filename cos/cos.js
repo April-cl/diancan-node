c9240aa5709815d890d0f7117878a8685dde836e
April-cl <xcl8202@gmail.com>
2021-10-28 16:39:49 +0800

    腾讯云cos对象存储sdk使用

--- cos/cos.js
+++ cos/cos.js
@@ -1,18 +1,40 @@
 const multer = require('@koa/multer')
+const COS = require('cos-nodejs-sdk-v5')
+
+var cos = new COS({
+  SecretId: 'AKID5xZu0pOgiTyC9qGPHtGL9hz0lGVUCdoj',
+  SecretKey: 'h8JDqEMEqBF2YomUw71f7JEsFLBk25sO',
+  Protocol: 'https:'
+})
+
+let Bucket = 'diancan-1307921259'
+let Region = 'ap-guangzhou'
+let codfun = function (filename, path) {
+  return new Promise((resolve, reject) => {
+    cos.uploadFile({
+      Bucket,
+      Region,
+      Key: filename,
+      FilePath: path
+    }).then(res => {
+      resolve(res.Location)
+    }).catch(err => {
+      reject(err)
+    })
+  })
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'upload/image')
   },
   filename: (req, file, cb) => {
-    console.log(file)
     let fileFormat = (file.originalname).split('.')
-    console.log(fileFormat)
     let num = `${Date.now()}-${Math.floor(Math.random()*10000000)}${'.'}${fileFormat[fileFormat.length-1]}`
-    console.log(num)
+    cb(null, num)
   }
 })
 
 const upload = multer({storage})
 
-module.exports = {upload}
+module.exports = {upload, codfun}
--- router/merchant-infor/infor.js
+++ router/merchant-infor/infor.js
@@ -1,8 +1,14 @@
+const result = require('../../config/result')
 const router = require('koa-router')()
-const {upload} = require('../../cos/cos.js')
+const {upload, codfun} = require('../../cos/cos.js')
 
 router.post('/uploadres', upload.single('file'), async ctx => {
-  console.log('infor.js')
+  try {
+    const res = await codfun(ctx.file.filename, ctx.file.path)
+    new result(ctx, 'SUCCESS', 200, 'https://'+res).answer()
+  } catch (e) {
+    new result(ctx, '上传失败，服务器发生错误', 500).answer()
+  }
 })
 
 module.exports = router.routes()
