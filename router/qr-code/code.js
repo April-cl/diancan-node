d1521fbcaae2e6cae6e9b60e8c0f4eb476fe5cb2
April-cl <xcl8202@gmail.com>
2022-01-13 02:09:36 +0800

    提交桌号到数据库，请求所有桌号数据

--- router/qr-code/code.js
+++ router/qr-code/code.js
@@ -11,9 +11,22 @@ const {Code} = require('../../config/code-img')
 router.post('/qrcode', new Auth().m, async ctx => {
   let {table} = ctx.request.body
   new postcode(ctx, table).start()
+  let time = moment().utcOffset(8).format('YYYY-MM-DD HH:mm:ss')
   try {
-    let res_code = await new getToken().qrcode(table)
-    const res_img = await buffer(Code(), res_code.data)
+    const query = `db.collection('table_qr_code').where({table:'${table}'}).get()`
+
+    const res = await new getToken().posteve(TripUrl, query)
+    console.log(res)
+    if (res.data.length > 0) {
+      new result(ctx, '该桌号已存在', 202).answer()
+    } else {
+      let res_code = await new getToken().qrcode(table)
+      const res_img = await buffer(Code(), res_code.data)
+      let code_image = 'https://' + res_img
+      let table_data = `db.collection('table_qr_code').add({data:{time:'${time}',table:'${table}',code:'${code_image}'}})`
+      await new getToken().posteve(AddUrl, table_data)
+      new result(ctx, '添加成功').answer()
+    }
   } catch (e) {}
 })
 
