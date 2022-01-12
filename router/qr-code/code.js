eadbc7ce260d3cdb352b7efa414d0554c82dffa0
April-cl <xcl8202@gmail.com>
2022-01-13 02:20:50 +0800

    请求所有桌号数据

--- router/qr-code/code.js
+++ router/qr-code/code.js
@@ -14,9 +14,7 @@ router.post('/qrcode', new Auth().m, async ctx => {
   let time = moment().utcOffset(8).format('YYYY-MM-DD HH:mm:ss')
   try {
     const query = `db.collection('table_qr_code').where({table:'${table}'}).get()`
-
     const res = await new getToken().posteve(TripUrl, query)
-    console.log(res)
     if (res.data.length > 0) {
       new result(ctx, '该桌号已存在', 202).answer()
     } else {
@@ -27,7 +25,24 @@ router.post('/qrcode', new Auth().m, async ctx => {
       await new getToken().posteve(AddUrl, table_data)
       new result(ctx, '添加成功').answer()
     }
-  } catch (e) {}
+  } catch (e) {
+    new result(ctx, '服务器发生错误', 500).answer()
+  }
+})
+
+router.get('/getqrcode', new Auth().m, async ctx => {
+  let {page} = ctx.query
+  let sk = Number(page) * 10
+  const query = `db.collection('table_qr_code').orderBy('time', 'desc').limit(10).skip(${sk}).get()`
+  try {
+    const res = await new getToken().posteve(TripUrl, query)
+    const data = res.data.map(item => JSON.parse(item))
+    const total = {total: res.pager.Total}
+    const array = {...{result: data}, ...total}
+    new result(ctx, 'SUCCESS', 200, array).answer()
+  } catch (e) {
+    new result(ctx, '服务器发生错误', 500).answer()
+  }
 })
 
 module.exports = router.routes()
